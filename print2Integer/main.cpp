aa6d8f7ef72a5b174a54bd0758bd70d7ee4153f1
yudihendrawan <yudihendrawan04@gmail.com>
2021-10-12 01:33:41 +0700

    second

---
+++ print2Integer/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int first, second;
+
+
+    cout << "Input First Number : ";
+    cin >> first;
+
+    cout << "Input Second Number : ";
+    cin >> second ;
+
+    cout << "the first number is : " << first << " , and the second number is : " << second << endl;
+
+
+    return 0;
+}
